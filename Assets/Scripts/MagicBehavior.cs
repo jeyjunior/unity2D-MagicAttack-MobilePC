b7dd10e08bde79c33de70bcf39c6ae37e5e43c1d
Jey Junior <jeyjuniordg@gmail.com>
2021-11-19 15:59:32 -0300

    Movimentacao base do objeto/magia spawnada (tiro)

--- Assets/Scripts/MagicBehavior.cs
+++ Assets/Scripts/MagicBehavior.cs
@@ -6,16 +6,10 @@ public class MagicBehavior : MonoBehaviour
 {
     public Player player;
     public float speed = 5f;
-    Vector3 dir;
-
-    Rigidbody2D rb2d;
-
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        rb2d = GetComponent<Rigidbody2D>();
-        dir = !player.mirrored ? new Vector3(-speed * Time.deltaTime, 0, 0) : new Vector3(speed * Time.deltaTime, 0, 0);
     }
     
     void FixedUpdate()
